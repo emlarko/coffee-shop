fc05a058abdfd4ff08856e94379c824b3f3985cd
Emily Larkin <emilylarkin@Emilys-MacBook-Air.local>
2023-06-02 12:54:47 +0100

    home and styling

--- client/src/Styled.js
+++ client/src/Styled.js
@@ -1,7 +1,34 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
+export const Container = styled.div`
+  @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&display=swap');
+ 
+  display: flex;
+  flex-direction: column;
 
+  & h1 {
+    font-family: "Bebas Neue", sans-serif;
+    font-size: 100px;
+  }
 
+  & h3 {
+    font-family: "Bebas Neue", sans-serif;
+    font-size: 50px;
+  }
 
+  & li {
+    font-family: 'Comfortaa', cursive;
+    font-size: 20px;
+    list-style: none;
+  }
 
-export {}
+`;
+
+export const Row = styled.div`
+  display: flex;
+`
+
+export const Col = styled.div`
+  flex:  ${(props) => props.size};
+`
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,54 @@
 import React from "react";
-// import ProductList from "../components/ProductList";
-// import CategoryMenu from "../components/CategoryMenu";
-// import Cart from "../components/Cart";
+import styled from 'styled-components'
+import { Container } from "../Styled";
+import { Row } from "../Styled"
+import { Col } from "../Styled"
+import CupWindow from '../assets/cup-window.jpg'
+import Martini from '../assets/espresso-martini.jpg'
+
+const Image = styled.img `
+  width: 100%;
+  position: relative;
+`
+const Title = styled.div `
+  padding: 10px;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  color: white;
+`
 
 const Home = () => {
   return (
-    <div className="container">
-      zehra
-    </div>
-  );
+    <Container>
+      <Row>
+        <Image src={CupWindow}/>
+        <Title>
+          <h1>Brunch</h1>
+          <h3>Coffee / Breakfast / Cake</h3>
+        </Title>
+        </Row>
+        <Row>
+        <Image src={Martini}/>
+        <Title>
+          <h1>Weekend Evening</h1>
+          <h3>Craft Beer / Cocktails / Street Food</h3>
+        </Title> 
+        </Row>
+        <Row>
+          <Col size={1}>
+          <h3>Opening Times</h3>
+          <ul>
+            <li>Monday - Thursday   8:30am - 4pm</li>
+            <li>Friday  8:30am - 10:00pm</li>
+            <li>Saturday 9am - 10:00pm</li>
+            <li>Sunday  9am - 3pm</li>
+            <li>Brunch is served Wednesday - Sunday 9am - 2pm</li>
+          </ul>
+          </Col>
+        </Row>
+      </Container>
+  )
 };
 
 export default Home;
